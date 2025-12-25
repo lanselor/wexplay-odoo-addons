f24415643d5976fdc3cdcaa475c5133fd5dae22e
AlexGesto <lanselor@gmail.com>
2025-12-26 00:34:40 +0100

    Actualización del botón, cambiamos enfoque. Correciones41

--- wexplay_repair/static/src/js/repair_order_expand_button.js
+++ wexplay_repair/static/src/js/repair_order_expand_button.js
@@ -4,18 +4,22 @@ import { ListController } from "@web/views/list/list_controller";
 // Guarda el original
 const originalGetStaticActionMenuItems = ListController.prototype.getStaticActionMenuItems;
 
+console.log("WEXPLAY: asset cargado (expand button)");
+
 patch(ListController.prototype, {
-
-        setup() {
-        if (this._super) this._super(...arguments);
-        window._wex_last_list_controller = this;
-        console.log("WEX: ListController expuesto", this.props?.resModel, this);
-        },
-
-
+   
+    setup() {
+    if (this._super) this._super(...arguments);
+    window._wex_last_list_controller = this;
+    console.log("WEX: ListController expuesto", this.props?.resModel, this);
+    },
+   
     getStaticActionMenuItems() {
         const items = originalGetStaticActionMenuItems.call(this, ...arguments) || {};
-       
+        
+
+        console.log("Estoy aqui");
+
 
         // Solo en repair.order
         if (this.props?.resModel !== "repair.order") {
