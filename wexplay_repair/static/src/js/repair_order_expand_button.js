0b0647b6724ab0f0514325fc3a417131467f8039
AlexGesto <lanselor@gmail.com>
2025-12-27 13:58:20 +0100

    Actualización del botón, cambiamos enfoque. Correciones51

--- wexplay_repair/static/src/js/repair_order_expand_button.js
+++ wexplay_repair/static/src/js/repair_order_expand_button.js
@@ -2,25 +2,20 @@ import { patch } from "@web/core/utils/patch";
 import { ListController } from "@web/views/list/list_controller";
 
 // Guarda originales ANTES del patch
-const originalSetup = ControlPanel.prototype.setup;
+const originalListSetup = ListController.prototype.setup;
 const originalGetStaticActionMenuItems = ListController.prototype.getStaticActionMenuItems;
 
-
 patch(ListController.prototype, {
-
     setup() {
-        originalSetup.call(this, ...arguments);
+        originalListSetup.call(this, ...arguments);
+
         window._wex_last_list_controller = this;
         window.wexExpandGroups = () => this.wexExpandGroups();
         window.wexCollapseGroups = () => this.wexCollapseGroups();
-        window._wex_cp = this;
 
-        // Log compacto para no inundar
-        console.log("WEX CP keys:", Object.keys(this.props || {}));
-        console.log("WEX CP props:", this.props);
+        console.log("WEX ListController resModel:", this.props?.resModel);
     },
 
-    // Handlers para los botones del Control Panel (siempre visibles)
     async wexExpandGroups() {
         if (this.props?.resModel !== "repair.order") return;
         await expandAllGroups(this);
@@ -31,32 +26,20 @@ patch(ListController.prototype, {
         await collapseAllGroups(this);
     },
 
-    // Mantengo esto por compatibilidad/si aún quieres la opción en el menú "Acciones" (bulk)
-    // Puedes eliminar este método si ya no lo necesitas.
     getStaticActionMenuItems() {
         const items = originalGetStaticActionMenuItems.call(this, ...arguments) || {};
 
-        if (this.props?.resModel !== "repair.order") {
-            return items;
-        }
+        if (this.props?.resModel !== "repair.order") return items;
 
-        if (!items.wex_expand_groups) {
-            items.wex_expand_groups = {
-                description: "Expandir grupos",
-                callback: async () => {
-                    await expandAllGroups(this);
-                },
-            };
-        }
+        items.wex_expand_groups ||= {
+            description: "Expandir grupos",
+            callback: async () => expandAllGroups(this),
+        };
 
-        if (!items.wex_collapse_groups) {
-            items.wex_collapse_groups = {
-                description: "Colapsar grupos",
-                callback: async () => {
-                    await collapseAllGroups(this);
-                },
-            };
-        }
+        items.wex_collapse_groups ||= {
+            description: "Colapsar grupos",
+            callback: async () => collapseAllGroups(this),
+        };
 
         return items;
     },
