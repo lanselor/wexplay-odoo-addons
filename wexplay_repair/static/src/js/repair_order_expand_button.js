0cd50715885f7b7fdd2e75005ea8a5fe03f10bb4
AlexGesto <lanselor@gmail.com>
2025-12-27 22:53:12 +0100

    Cambios en la lógica para evitar errores de expansióna

--- wexplay_repair/static/src/js/repair_order_expand_button.js
+++ wexplay_repair/static/src/js/repair_order_expand_button.js
@@ -57,6 +57,9 @@ try {
 
         // ¿Hay al menos un grupo plegado? (caret a la derecha)
         _wexHasFoldedGroups() {
+
+            console.warn("llego hasta aquí");
+
             const headers = document.querySelectorAll("tr.o_group_has_content.o_group_header");
             return Array.from(headers).some(tr =>
                 tr.querySelector(".o_group_caret")?.classList.contains("fa-caret-right")
