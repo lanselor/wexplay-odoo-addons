ef9b38e5f57711520590d367a029cf3700a8ad5b
AlexGesto <lanselor@gmail.com>
2025-12-27 17:31:21 +0100

    Actualización del botón, hasta los huevos...

--- wexplay_repair/static/src/js/repair_order_expand_button.js
+++ wexplay_repair/static/src/js/repair_order_expand_button.js
@@ -1,3 +1,10 @@
 /** @odoo-module **/
 
+
+import { ListController } from "@web/views/list/list_controller";
+
 console.log("WEXPLAY: repair_order_expand_button cargado");
+
+if (ListController) {
+    console.log("ListController disponible");
+}
