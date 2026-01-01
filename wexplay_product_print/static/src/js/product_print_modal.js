69fc7e594a10ed329abc827e627b5ac81b5570e3
AlexGesto <lanselor@gmail.com>
2026-01-01 23:31:15 +0100

    Problemas con el uso de controladores antiguos.

--- wexplay_product_print/static/src/js/product_print_modal.js
+++ wexplay_product_print/static/src/js/product_print_modal.js
@@ -10,7 +10,7 @@ class PrintCenterModal extends Component {
     setup() {
         
         this.notification = useService("notification");
-        this.rpc = useService("orm");
+        this.orm = useService("orm");
         this.actionService = useService("action");
         this.dialog = useService("dialog");
     }
