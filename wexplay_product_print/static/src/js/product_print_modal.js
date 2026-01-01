f162c9a5843760207a9b353fc276840a9f0c2905
AlexGesto <lanselor@gmail.com>
2026-01-01 22:30:45 +0100

    Error en la carga del modal en el JS

--- wexplay_product_print/static/src/js/product_print_modal.js
+++ wexplay_product_print/static/src/js/product_print_modal.js
@@ -22,6 +22,12 @@ PrintCenterModal.template = "wexplay_product_print.PrintCenterModal";
 
 registry.category("actions").add("wexplay_product_print.print_center", async (env) => {
     console.log("WEXPLAY_PRINT: handler ejecutado");
-    env.services.notification.add("Print Center: acción ejecutada", { type: "info" });
-    env.services.dialog.add(PrintCenterModal, {});
+    env.services.notification.add("Wexplay Print: acción ejecutada", { type: "info" });
+
+    const close = env.services.dialog.add(PrintCenterModal, {
+        close: null, // placeholder, lo reescribimos abajo
+    });
+
+    // En algunas versiones add() devuelve una función close, en otras un id.
+    // Para hacerlo universal, volvemos a abrir de forma simple:
 });
