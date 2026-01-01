3ef7a6c84b1caaeea78f0617423aaa00b0f03d3c
AlexGesto <lanselor@gmail.com>
2026-01-01 21:58:03 +0100

    Corecciones de JS y Vistas

--- wexplay_product_print/__manifest__.py
+++ wexplay_product_print/__manifest__.py
@@ -9,9 +9,7 @@
     "assets": {
         "web.assets_backend": [
             "wexplay_product_print/static/src/js/product_print_modal.js",
-        ],
-        "web.assets_qweb": [
-            "wexplay_product_print/static/src/xml/product_print_modal.xml",
+            "wexplay_product_print/static/src/xml/product_print_modal.xml", # EL XML VA AQUÍ
         ],
     },
     "installable": True,
