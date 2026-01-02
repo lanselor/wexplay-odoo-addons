d80492661de4ebdd848161ecb62bfd7ffb0d2e92
Alex Gesto <lanselor@gmail.com>
2026-01-02 18:06:49 +0100

    Incluimos los JS de QZ Tray, primera prueba de funcionalidad- Error

--- wexplay_product_print/__manifest__.py
+++ wexplay_product_print/__manifest__.py
@@ -8,8 +8,8 @@
     ],
     "assets": {
         "web.assets_backend": [
-            #"wexplay_product_print/static/lib/qz-tray.js",
-            #"wexplay_product_print/static/src/js/qz_print.js",
+            "wexplay_product_print/static/lib/qz-tray.js",
+            "wexplay_product_print/static/src/js/qz_print.js",
             "wexplay_product_print/static/src/js/product_print_modal.js",
             # Workaround: forzar carga del template en el bundle backend (evita Missing template)
             "wexplay_product_print/static/src/xml/product_print_modal.xml",
