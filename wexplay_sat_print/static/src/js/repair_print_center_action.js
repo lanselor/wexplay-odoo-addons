fe179299656a19a7a289f8511bb0b144aa6a2b64
alexgesto <algesto1987@gmail.com>
2026-01-10 14:21:22 +0100

    Errores varios de configuración en XML y otros archivos

--- wexplay_sat_print/static/src/js/repair_print_center_action.js
+++ wexplay_sat_print/static/src/js/repair_print_center_action.js
@@ -2,7 +2,7 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { SatPrintCenterModal } from "@wexplay_sat_print/static/src/js/repair_print_center_modal.js";
+import { SatPrintCenterModal } from "@wexplay_sat_print/static/src/js/repair_print_center_modal";
 
 
 console.log("WEXPLAY_SAT_PRINT: repair_print_center_action.js cargado");
