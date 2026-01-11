04c4f54a462ca6ccfb28334ec85b8700f25c5619
alexgesto <algesto1987@gmail.com>
2026-01-11 21:15:46 +0100

    Añadimos assets js especificos para el módulo de ajustes, añadimos funciones para comprobar estados de impresoras

--- wexplay_sat_print/models/res_config_settings.py
+++ wexplay_sat_print/models/res_config_settings.py
@@ -1,32 +1,16 @@
-from odoo import fields, models
-
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    # TEMPORAL: usar config_parameter para no depender de res.company durante el arranque/upgrade
-    wex_qz_label_printer = fields.Char(
-        string="Impresora etiquetas (QZ)",
-        config_parameter="wexplay_sat_print.wex_qz_label_printer",
-    )
-    wex_qz_thermal_printer = fields.Char(
-        string="Impresora térmica (QZ)",
-        config_parameter="wexplay_sat_print.wex_qz_thermal_printer",
-    )
-    wex_qz_a4_printer = fields.Char(
-        string="Impresora A4 (QZ)",
-        config_parameter="wexplay_sat_print.wex_qz_a4_printer",
-    )
+    # Cambia config_parameter por related para apuntar a res.company
+    wex_qz_label_printer = fields.Char(related="company_id.wex_qz_label_printer", readonly=False)
+    wex_qz_thermal_printer = fields.Char(related="company_id.wex_qz_thermal_printer", readonly=False)
+    wex_qz_a4_printer = fields.Char(related="company_id.wex_qz_a4_printer", readonly=False)
+    wex_qz_debug = fields.Boolean(related="company_id.wex_qz_debug", readonly=False)
+    wex_qz_allow_fallback = fields.Boolean(related="company_id.wex_qz_allow_fallback", readonly=False)
+    
+    # Estos campos de diagnóstico también deben ser related para mostrar los datos de la empresa
+    wex_qz_last_test_ok = fields.Boolean(related="company_id.wex_qz_last_test_ok", readonly=True)
+    wex_qz_last_test_at = fields.Datetime(related="company_id.wex_qz_last_test_at", readonly=True)
+    wex_qz_last_test_user_id = fields.Many2one(related="company_id.wex_qz_last_test_user_id", readonly=True)
 
-    wex_qz_debug = fields.Boolean(
-        string="Debug QZ",
-        config_parameter="wexplay_sat_print.wex_qz_debug",
-        default=False,
-    )
-    wex_qz_allow_fallback = fields.Boolean(
-        string="Permitir fallback a impresora por defecto del sistema",
-        config_parameter="wexplay_sat_print.wex_qz_allow_fallback",
-        default=True,
-    )
-
-    # Campo dummy para el widget
     wex_qz_ui = fields.Char(string="QZ UI", readonly=True)
