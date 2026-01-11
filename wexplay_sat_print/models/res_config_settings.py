b50f0cc94b51e5cb457b2e958ab1d9d84390f4de
alexgesto <algesto1987@gmail.com>
2026-01-11 19:17:40 +0100

    Añadimos assets js especificos para el módulo de ajustes, añadimos funciones para comprobar estados de impresoras

--- wexplay_sat_print/models/res_config_settings.py
+++ wexplay_sat_print/models/res_config_settings.py
@@ -1,18 +1,18 @@
 from odoo import fields, models
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
+   # _inherit = "res.config.settings"
 
-    wex_qz_label_printer = fields.Char(related="company_id.wex_qz_label_printer", readonly=False)
-    wex_qz_thermal_printer = fields.Char(related="company_id.wex_qz_thermal_printer", readonly=False)
-    wex_qz_a4_printer = fields.Char(related="company_id.wex_qz_a4_printer", readonly=False)
+    #wex_qz_label_printer = fields.Char(related="company_id.wex_qz_label_printer", readonly=False)
+   # wex_qz_thermal_printer = fields.Char(related="company_id.wex_qz_thermal_printer", readonly=False)
+    #wex_qz_a4_printer = fields.Char(related="company_id.wex_qz_a4_printer", readonly=False)
 
-    wex_qz_debug = fields.Boolean(related="company_id.wex_qz_debug", readonly=False)
-    wex_qz_allow_fallback = fields.Boolean(related="company_id.wex_qz_allow_fallback", readonly=False)
+   # wex_qz_debug = fields.Boolean(related="company_id.wex_qz_debug", readonly=False)
+   # wex_qz_allow_fallback = fields.Boolean(related="company_id.wex_qz_allow_fallback", readonly=False)
 
-    wex_qz_last_test_ok = fields.Boolean(related="company_id.wex_qz_last_test_ok", readonly=True)
-    wex_qz_last_test_at = fields.Datetime(related="company_id.wex_qz_last_test_at", readonly=True)
-    wex_qz_last_test_user_id = fields.Many2one(related="company_id.wex_qz_last_test_user_id", readonly=True)
+    #wex_qz_last_test_ok = fields.Boolean(related="company_id.wex_qz_last_test_ok", readonly=True)
+   # wex_qz_last_test_at = fields.Datetime(related="company_id.wex_qz_last_test_at", readonly=True)
+    #wex_qz_last_test_user_id = fields.Many2one(related="company_id.wex_qz_last_test_user_id", readonly=True)
 
     # Campo “dummy” para enganchar el widget (no se guarda)
-    wex_qz_ui = fields.Char(string="QZ UI", readonly=True)
+    #wex_qz_ui = fields.Char(string="QZ UI", readonly=True)
