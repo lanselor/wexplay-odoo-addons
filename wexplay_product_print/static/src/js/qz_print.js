b95a38053fdc7fc11d13da9b46bfda3115540cb0
alexgesto <algesto1987@gmail.com>
2026-01-17 12:54:32 +0100

    Mejorando el diseñ del Modal de productos, añadiendo opcion de impresión por cantidad

--- wexplay_product_print/static/src/js/qz_print.js
+++ wexplay_product_print/static/src/js/qz_print.js
@@ -1,5 +1,5 @@
 /** @odoo-module **/
-console.log("🔥🔥🔥 QZ_PRINT VERSION 29 (CONFIG BY KIND) 🔥🔥🔥");
+console.log("🔥🔥🔥 QZ_PRINT VERSION 30 (CONFIG BY KIND) 🔥🔥🔥");
 
 import { browser } from "@web/core/browser/browser";
 
