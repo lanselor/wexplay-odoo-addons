b7df8fe41fc7caaee8adbc6de612e4ace10c33df
alexgesto <algesto1987@gmail.com>
2026-02-11 23:00:45 +0100

    Añadido nuevo módulo de Whatsapp

---
+++ wex_whatsapp_chatter/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Wex WhatsApp Chatter (Base)",
+    "version": "18.0.1.0.0",
+    "category": "Productivity",
+    "summary": "Base: groups + WhatsApp menu (no templates/wizard yet).",
+    "license": "LGPL-3",
+    "author": "Wexplay",
+    "depends": [
+        "base",
+        "mail",
+    ],
+    "data": [
+        "security/whatsapp_security.xml",
+        "views/whatsapp_menus.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
